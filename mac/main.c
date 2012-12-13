4b0e7076b71898804512354e23aeedb1524caaf6
Alex Leigh <leigh@alexleigh.me>
2012-12-13 03:34:48 -0500

    Initial commit

---
+++ mac/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  mac
+//
+//  Created by Alex Leigh on 12/13/12.
+//  Copyright (c) 2012 MAC App Center. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("MAC App Center\n");
+    return 0;
+}
+
